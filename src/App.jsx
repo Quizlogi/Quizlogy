1794c8589b1bed61995494ab20553f0df45ab4fc
zalfarmdhn <zalfarmdhn1011@gmail.com>
2024-05-16 23:21:24 +0700

    setup material-tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from '@material-tailwind/react';
 import React from 'react';
 
 function App() {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <h1 className='text-violet-600'>Hello world!</h1>
+      <Button>Button</Button>
     </div>
   )
 }
