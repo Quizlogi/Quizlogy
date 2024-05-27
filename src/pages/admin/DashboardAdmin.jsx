82ad6eee2f28eca62aab4a3333f78df9c7e96e0c
vinaanurfauziah <vinaanurfauziah@gmail.com>
2024-05-27 07:35:25 +0700

    layouting di pages

--- src/pages/admin/DashboardAdmin.jsx
+++ src/pages/admin/DashboardAdmin.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { SidebarAdmin } from '../components/admin/SidebarAdmin';
-import Card from '../components/Card';
-import EditAdmin from '../components/admin/EditAdmin';
+import { SidebarAdmin } from '../../components/admin/SidebarAdmin';
+import Card from '../../components/Card';
+import EditAdmin from '../../components/admin/EditAdmin';
 
 export default function DashboardAdmin() {
   return (
---
+++ src/pages/penguji/DashboardPenguji.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Sidebar } from '../../components/Sidebar';
+import Card from '../../components/Card';
+import Edit from '../../components/Edit';
+
+export default function DashboardPenguji() {
+  return (
+    <div className='bg-gray-100 flex flex-col md:flex-row h-dvh'>
+      <Sidebar />
+      <div className='flex-grow p-4'>
+        <Card />
+        <Edit />
+      </div>
+    </div>
+  );
+}
