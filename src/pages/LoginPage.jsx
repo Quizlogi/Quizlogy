571ebb1cc9a9d27b290348569b2e68aa594ffb62
vinaanurfauziah <vinaanurfauziah@gmail.com>
2024-06-07 19:56:39 +0700

    pasang logo di loginpage & registerpage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,6 +3,8 @@ import { useStore } from '../states/auth';
 import useInput from '../hooks/useInput';
 import { Toaster } from 'react-hot-toast';
 import coverImage from '/cover.jpg';
+import quizlogyIcon from '/quizlogy-logo-horizontal.png';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
   const [email, setEmail] = useInput('');
@@ -39,9 +41,16 @@ const LoginPage = () => {
           />
         </div>
         <div className='w-1/2 h-full bg-[#F5F5F5] flex flex-col p-20 justify-between items-center'>
-          <h1 className='w-full max-w-[500px] mx-auto text-xl text-[#060606] font-semibold mr-auto'>
+          {/* <h1 className='w-full max-w-[500px] mx-auto text-xl text-[#060606] font-semibold mr-auto'>
             Quizlogy
-          </h1>
+          </h1> */}
+          <Link to="/">
+          <img
+            src={quizlogyIcon}
+            alt="Quizlogy Icon"
+            className='w-full max-w-[130px] mx-auto text-xl mr-auto' 
+          />
+          </Link>
           <LoginInput
             email={email}
             setEmail={setEmail}
