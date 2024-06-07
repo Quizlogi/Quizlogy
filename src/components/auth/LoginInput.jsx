4a6ae3834ecf9c4f8845eee2d714897dda3f0dbd
vinaanurfauziah <vinaanurfauziah@gmail.com>
2024-06-07 19:12:56 +0700

    re-design register page

--- src/components/auth/LoginInput.jsx
+++ src/components/auth/LoginInput.jsx
@@ -8,7 +8,7 @@ export function LoginInput({
   onSubmit,
 }) {
   return (
-    <div className='w-full flex flex-col max-w-[500px]'>
+    <div className='w-full flex flex-col max-w-[500px] mb-24'>
       {' '}
       <div className='w-full flex flex-col mb-10'>
         <h3 className='text-3xl font-semibold mb-2'>Login</h3>
@@ -38,26 +38,13 @@ export function LoginInput({
             required
           />
         </div>
-        {/* <div className='w-full flex items-center justify-between'>
-          <div className='w-full flex items-center'>
-            <input type='checkbox' className='w-4 h-4 mr-2' />
-            <p className='text-sm'>Remember me</p>
-          </div>
-          <p className='text-sm font-medium whitespace-nowrap cursor-pointer underline underline-offset-2'>
-            Forgot Password?
-          </p>
-        </div> */}
+
         <div className='w-full flex flex-col my-4'>
           <button
             type='submit'
-            className='w-full text-white my-2 font-semibold bg-[#060606] rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>
+            className='w-full text-white my-2 font-semibold bg-secondary rounded-md p-4 text-center flex items-center justify-center hover:bg-primary transition-all duration-300 cursor-pointer'>
             Log In
           </button>{' '}
-          {/* <button
-            className='w-full text-[#060606] my-2 font-semibold bg-white border border-black rounded-md p-4 text-center flex items-center justify-center cursor-pointer '
-            type='submit'>
-            Register
-          </button> */}
         </div>
       </form>
       <div className='w-full flex items-center justify-center'>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,8 @@
-import { NavbarAuth } from "../components/navbar/NavbarAuth";
+// import { NavbarAuth } from "../components/navbar/NavbarAuth";
 import { RegisterInput } from "../components/auth/RegisterInput";
 import { useStore } from "../states/auth";
 import useInput from "../hooks/useInput";
+import coverImage from '/cover.jpg';
 
 const RegisterPage = () => {
   const [name, setName] = useInput("");
@@ -12,7 +13,7 @@ const RegisterPage = () => {
   const { register } = useStore((state) => ({
     user: state.user,
     register: state.register,
-  }));
+  })); 
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -23,10 +24,28 @@ const RegisterPage = () => {
 
 
   return (
-    <div className="bg-[#ECEFF1] h-screen overflow-hidden">
-      <NavbarAuth type="register" />
-      <div className="flex justify-center items-center h-full">
-        <RegisterInput
+    <div className='w-full h-screen flex items-start'>
+        {/* <NavbarAuth type="login" /> */}
+        <div className='relative w-1/2 h-full flex flex-col'>
+          <div className='absolute top-[20%] left-[10%] flex flex-col'>
+            <h1 className='text-4xl text-white font-bold my-4'>
+            Create engaging quizzes effortlessly
+            </h1>
+            <p className='text-xl text-white font-normal'>
+            Join now and inspire learners everywhere</p>
+          </div>
+ 
+          <img
+            src={coverImage}
+            className='w-full h-full object-cover'
+            alt='cover'
+          />
+        </div>
+        <div className='w-1/2 h-full bg-[#F5F5F5] flex flex-col p-20 justify-between items-center'>
+          <h1 className='w-full max-w-[500px] mx-auto text-xl text-[#060606] font-semibold mr-auto'>
+            Quizlogy
+          </h1>
+          <RegisterInput
           name={name}
           setName={setName}
           username={username}
@@ -37,8 +56,8 @@ const RegisterPage = () => {
           setPassword={setPassword}
           onSubmit={onSubmit}
         />
+        </div>
       </div>
-    </div>
   );
 };
 
