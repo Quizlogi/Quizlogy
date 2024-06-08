03baa82fc037b19b047cdc8ba18e438a5b4740eb
vinaanurfauziah <vinaanurfauziah@gmail.com>
2024-06-08 15:24:55 +0700

    fix responsive register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -25,9 +25,9 @@ const RegisterPage = () => {
 
 
   return (
-    <div className='w-full h-screen flex items-start'>
-        <div className='relative w-1/2 h-full flex flex-col'>
-          <div className='absolute top-[20%] left-[10%] flex flex-col'>
+    <div className='w-full h-screen flex flex-col md:flex-row items-start'>
+        <div className='relative w-full md:w-1/2 md:h-full flex flex-col'>
+          <div className='absolute top-[20%] left-[10%] flex flex-col hidden md:block'>
             <h1 className='text-4xl text-white font-bold my-4'>
             Create engaging quizzes effortlessly
             </h1>
@@ -37,11 +37,11 @@ const RegisterPage = () => {
  
           <img
             src={coverImage}
-            className='w-full h-full object-cover'
+            className='w-full h-full object-cover hidden md:block'
             alt='cover'
           />
         </div>
-        <div className='w-1/2 h-full bg-[#F5F5F5] flex flex-col p-20 justify-between items-center'>
+        <div className='w-full md:w-1/2 h-full bg-[#F5F5F5] flex flex-col p-8 md:p-10 justify-between items-center sm:p-4 sm:items-start'>
           {/* <h1 className='w-full max-w-[500px] mx-auto text-xl text-[#060606] font-semibold mr-auto'>
             Quizlogy
           </h1> */}
